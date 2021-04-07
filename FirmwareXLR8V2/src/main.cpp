020eb16252e71bd2120fe7405a937fe142a5d190
Nicolás Useche <u20171154918@usco.edu.co>
2021-04-07 11:35:43 -0500

    first commit

---
+++ FirmwareXLR8V2/src/main.cpp
@@ -0,0 +1,35 @@
+/*
+XLR8 V2
+
+TODO:
+----------------
+
+Pin Mapping:
+Physical Pin  Arduino Pin    Port Pin     Function
+======================================
+2                              PD4          BTN1
+10                             PD6          BTN2
+11                             PD7          BTN3
+12                             PB0          BTN4
+13                             PB1          BTN5
+1                              PD3          BTN6
+23                             PC0          BTN7
+24                             PC1          BTN8
+25                             PC2          BTN9
+13                             PB1          CE
+14                             PB2          CSN
+17                             PB5          SCK
+15                             PB3          MOSI
+16                             PB4          MISO
+*/
+#include <Arduino.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
